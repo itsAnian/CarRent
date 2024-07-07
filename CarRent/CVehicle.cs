26753f2e1cb2e9e45b93008981c1fc759e26afab
itsAnian <anian.seidl@gmail.com>
2024-07-07 10:05:44 +0200

    Add class CVehicle Add class Program
    CVehicle consists of
     - Constructor
     - ToString, which contains all information to one car
     - CalculateRentalCost
    
    Program
     - creates an array of object and fills this array
    - calls ToString for each car
    - Calculates RentalCost for one car

---
+++ CarRent/CVehicle.cs
@@ -0,0 +1,47 @@
+namespace CarRent;
+public class CVehicle
+{
+    private int ID;
+    private string Manufacturer;
+    private string Model;
+    private short Year;
+    private int Mileage;
+    private bool Availability;
+    private float RentalCost;
+
+    public CVehicle(int id, string manufacturer, string model, short year, int milage, bool availability, float rentalCost)
+    {
+        ID = id;
+        Manufacturer = manufacturer;
+        Model = model;
+        Year = year;
+        Mileage = milage;
+        Availability = availability;
+        RentalCost = rentalCost;
+    }
+
+    public float CalculateRentalCost()
+    {
+        float prize;
+        Console.WriteLine("Enter how much days you wanna rent the car");
+        int days = Convert.ToInt16(Console.ReadLine());
+        prize = RentalCost * days;
+        Console.WriteLine("You have to pay: " + prize + "€");
+        return prize;
+    }
+
+    public string ToString()
+    {
+        string information;
+        information = $"""
+                      ID = {ID} 
+                      Manufacturer = {Manufacturer}
+                      Model = {Model}
+                      Year = {Year}
+                      Mileage = {Mileage}
+                      Availability = {Availability} 
+                      RentalCost = {RentalCost}
+                      """;
+        return information;
+    }
+}
---
+++ CarRent/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CarRent;
+
+class Program
+{ 
+    static void Main() {
+        int[] IDs = {1, 2, 3, 4, 5};
+        string[] Manufacturers = { "Mercedes", "Ford", "VW", "Ferrari", "Kia"};
+        string[] Models = { "AMG", "Fusion", "Golf", "911", "Carnival" };
+        short[] Years = { 2022, 2009, 1999, 2015, 2012};
+        int[] Milages = { 100000, 130000, 170000, 30000, 250000 };
+        bool[] Availability = { true, true, true, true, true };
+        float[] RentalCost = { 59.30f, 49.20f, 29.99f, 77.77f, 29.74f };
+
+        CVehicle[] cars = new CVehicle[5];
+        
+        for (int i = 0; i < IDs.Length; i++)
+        {
+            cars[i] = new CVehicle(IDs[i], Manufacturers[i], Models[i], Years[i], Milages[i], Availability[i], RentalCost[i]);
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car.ToString());
+        }
+        
+        cars[4].CalculateRentalCost();
+    }
+}
